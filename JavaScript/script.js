deb293f7b36cdd4a378dcb92d6edd3a2f3ad2198
RAJASHIVA <104512497+rajashiva2525@users.noreply.github.com>
2023-02-06 14:37:41 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Full Stack developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
---
+++ JavaScript/style-switcher.js
@@ -0,0 +1,14 @@
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load",() =>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+} )
